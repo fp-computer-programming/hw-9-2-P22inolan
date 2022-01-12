de67db947122cf94076aa18d688fd65f22585324
P22inolan <p22inolan@fairfieldprep.org>
2022-01-12 10:20:32 -0500

    WIP

---
+++ hw9-2-1.py
@@ -0,0 +1,9 @@
+# Author: IBN (AMDG) 1/12/2022
+
+last_initials = ["B", "D", "H", "E", "G", "G", "H", "R", "M", "L", "I", "I", "N", "N", "O", "P", "P", "X", "T", "S", "S", "P"]
+rows = [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"], ["Aiden", "Ian", "Colin" "Tim", "Cam"], ["Larry", "Spencer", "Chris", "Ryan", "Nolan"]]
+blank = []
+
+for row in rows:
+    for x in row:
+        blank.append(x + )
