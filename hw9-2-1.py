149b9773da43456125fa7f7783399d9a6e0a7e1e
P22inolan <p22inolan@fairfieldprep.org>
2022-01-13 10:13:58 -0500

    Submit 9-2

--- hw9-2-1.py
+++ hw9-2-1.py
@@ -1,9 +1,13 @@
 # Author: IBN (AMDG) 1/12/2022
 
+from types import LambdaType
+
+
 last_initials = ["B", "D", "H", "E", "G", "G", "H", "R", "M", "L", "I", "I", "N", "N", "O", "P", "P", "X", "T", "S", "S", "P"]
-rows = [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"], ["Aiden", "Ian", "Colin" "Tim", "Cam"], ["Larry", "Spencer", "Chris", "Ryan", "Nolan"]]
-blank = []
+rows = [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"], ["Aiden", "Ian", "Colin", "Tim", "Cam"], ["Larry", "Spencer", "Chris", "Ryan", "Nolan"]]
 
 for row in rows:
-    for x in row:
-        blank.append(x + )
+        for i, v in enumerate(row):
+            row[i] = "{0} {1}.".format(v, last_initials[0])
+            del last_initials[0]
+print(rows)
